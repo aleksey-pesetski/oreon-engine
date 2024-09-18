6f539fd2f3d24c4a7e228d4fb5ce5473f76e0357
Aliaksei Piasetski <aleksey.pesetski@gmail.com>
2024-09-18 15:34:28 +0200

    feat: update to gradle

--- oreonengine/oe-vk-api/src/main/java/org/oreon/core/vk/wrapper/image/VkImageHelper.java
+++ oreonengine/oe-vk-api/src/main/java/org/oreon/core/vk/wrapper/image/VkImageHelper.java
@@ -1,5 +1,6 @@
 package org.oreon.core.vk.wrapper.image;
 
+import static org.lwjgl.stb.STBImage.STBI_rgb_alpha;
 import static org.lwjgl.vulkan.VK10.VK_ACCESS_TRANSFER_READ_BIT;
 import static org.lwjgl.vulkan.VK10.VK_ACCESS_TRANSFER_WRITE_BIT;
 import static org.lwjgl.vulkan.VK10.VK_COMMAND_BUFFER_LEVEL_PRIMARY;
@@ -19,8 +20,6 @@ import static org.lwjgl.vulkan.VK10.VK_STRUCTURE_TYPE_IMAGE_MEMORY_BARRIER;
 import static org.lwjgl.vulkan.VK10.vkCmdBlitImage;
 import static org.lwjgl.vulkan.VK10.vkCmdPipelineBarrier;
 
-import java.nio.ByteBuffer;
-
 import org.lwjgl.vulkan.VkDevice;
 import org.lwjgl.vulkan.VkImageBlit;
 import org.lwjgl.vulkan.VkImageMemoryBarrier;
@@ -28,6 +27,7 @@ import org.lwjgl.vulkan.VkOffset3D;
 import org.lwjgl.vulkan.VkPhysicalDeviceMemoryProperties;
 import org.lwjgl.vulkan.VkQueue;
 import org.oreon.core.image.ImageMetaData;
+import org.oreon.core.util.ResourceLoaderUtils;
 import org.oreon.core.util.Util;
 import org.oreon.core.vk.command.CommandBuffer;
 import org.oreon.core.vk.command.SubmitInfo;
@@ -39,49 +39,50 @@ import org.oreon.core.vk.wrapper.command.ImageCopyCmdBuffer;
 import org.oreon.core.vk.wrapper.command.ImageLayoutTransitionCmdBuffer;
 
 public class VkImageHelper {
-	
+
 	public static VkImage loadImageFromFile(VkDevice device,
 			VkPhysicalDeviceMemoryProperties memoryProperties,
 			long commandPool, VkQueue queue, String file,
 			int usage, int layout, int dstAccesMask, int dstStageMask,
 			int dstQueueFamilyIndex){
-		
+
 		return loadImage(device, memoryProperties, commandPool, queue,
 				file, usage, layout, dstAccesMask, dstStageMask,
 				dstQueueFamilyIndex, false);
 	}
-	
+
 	public static VkImage loadImageFromFileMipmap(VkDevice device,
 			VkPhysicalDeviceMemoryProperties memoryProperties,
 			long commandPool, VkQueue queue, String file,
 			int usage, int layout, int dstAccessMask, int dstStageMask,
 			int dstQueueFamilyIndex){
-		
-		
+
+
 		return loadImage(device, memoryProperties, commandPool, queue,
 				file, usage, layout, dstAccessMask, dstStageMask,
 				dstQueueFamilyIndex, true);
 	}
-	
+
 	private static VkImage loadImage(VkDevice device,
 			VkPhysicalDeviceMemoryProperties memoryProperties, long commandPool, VkQueue queue,
 			String file, int usage, int finalLayout, int dstAccessMask, int dstStageMask,
 			int dstQueueFamilyIndex, boolean mipmap){
-		
+
 		ImageMetaData metaData = VkImageLoader.getImageMetaData(file);
-		ByteBuffer imageBuffer = VkImageLoader.decodeImage(file);
-		
+		//ByteBuffer imageBuffer = VkImageLoader.decodeImage(file);
+		var imageBuffer = ResourceLoaderUtils.loadImageToByteBuffer(file, STBI_rgb_alpha);
+
 		StagingBuffer stagingBuffer = new StagingBuffer(device, memoryProperties, imageBuffer);
-		
+
 		int mipLevels = mipmap ? Util.getMipLevelCount(metaData) : 1;
-		
+
 		VkImage image = new Image2DDeviceLocal(device,
 				memoryProperties, metaData.getWidth(), metaData.getHeight(),
 				VK_FORMAT_R8G8B8A8_UNORM, usage | VK_IMAGE_USAGE_TRANSFER_DST_BIT |
 				// if mipmap == true, usage flag TRANSFER_SRC_BIT is necessarry for mipmap generation
 				(mipmap ? VK_IMAGE_USAGE_TRANSFER_SRC_BIT : 0),
 				1, mipLevels, metaData);
-	    
+
 	    // transition layout barrier
 	    ImageLayoutTransitionCmdBuffer imageMemoryBarrierLayout0 = new ImageLayoutTransitionCmdBuffer(device, commandPool);
 	    imageMemoryBarrierLayout0.record(image.getHandle(),
@@ -90,12 +91,12 @@ public class VkImageHelper {
 	    		0, VK_ACCESS_TRANSFER_WRITE_BIT, VK_PIPELINE_STAGE_TOP_OF_PIPE_BIT,
 	    		VK_PIPELINE_STAGE_TRANSFER_BIT, mipLevels);
 	    imageMemoryBarrierLayout0.submit(queue);
-	    
+
 	    // copy buffer to image
 	    ImageCopyCmdBuffer imageCopyCmd = new ImageCopyCmdBuffer(device, commandPool);
 	    imageCopyCmd.record(stagingBuffer.getHandle(), image.getHandle(), metaData);
 	    imageCopyCmd.submit(queue);
-	    
+
 	    // transition layout barrier
 	    ImageLayoutTransitionCmdBuffer imageMemoryBarrierLayout1 = new ImageLayoutTransitionCmdBuffer(device, commandPool);
 		imageMemoryBarrierLayout1.record(image.getHandle(),
@@ -105,7 +106,7 @@ public class VkImageHelper {
 				VK_PIPELINE_STAGE_TRANSFER_BIT, dstStageMask, mipLevels);
 		Fence fence = new Fence(device);
 		imageMemoryBarrierLayout1.submit(queue, fence);
-		
+
 		if (mipmap){
 			generateMipmap(device, commandPool, queue,
 					image.getHandle(), metaData.getWidth(), metaData.getHeight(), mipLevels,
@@ -114,28 +115,28 @@ public class VkImageHelper {
 					dstStageMask, dstStageMask,
 					dstQueueFamilyIndex);
 		}
-		
+
 		imageMemoryBarrierLayout0.destroy();
 		imageMemoryBarrierLayout1.destroy();
 		imageCopyCmd.destroy();
 		stagingBuffer.destroy();
 		fence.destroy();
-		
+
 		return image;
 	}
-	
+
 	public static void generateMipmap(VkDevice device, long commandPool, VkQueue queue,
 			long image, int width, int height, int mipLevels,
 			int initialLayout, int finalLayout,
-			int initialSrcAccesMask, int finalDstAccesMask, 
+			int initialSrcAccesMask, int finalDstAccesMask,
 			int initialSrcStageMask, int finalDstStageMask,
 			int dstQueueFamilyIndex){
-		
+
 		CommandBuffer commandBuffer = new CommandBuffer(device,
 				commandPool, VK_COMMAND_BUFFER_LEVEL_PRIMARY);
-		
+
 		commandBuffer.beginRecord(VK_COMMAND_BUFFER_USAGE_ONE_TIME_SUBMIT_BIT);
-		
+
 		VkImageMemoryBarrier.Buffer barrier = VkImageMemoryBarrier.calloc(1)
 				.sType(VK_STRUCTURE_TYPE_IMAGE_MEMORY_BARRIER)
 				.srcQueueFamilyIndex(VK_QUEUE_FAMILY_IGNORED)
@@ -156,37 +157,37 @@ public class VkImageHelper {
 				initialSrcStageMask,
 				VK_PIPELINE_STAGE_TRANSFER_BIT,
 				barrier);
-		
+
 		int mipWidth = width;
 		int mipHeight = height;
-				
+
 		for (int i = 1; i < mipLevels; i++) {
-			
+
 			barrier.oldLayout(VK_IMAGE_LAYOUT_UNDEFINED)
 				.newLayout(VK_IMAGE_LAYOUT_TRANSFER_DST_OPTIMAL)
 				.srcAccessMask(0)
 				.dstAccessMask(VK_ACCESS_TRANSFER_WRITE_BIT);
 				barrier.subresourceRange()
 					.baseMipLevel(i);
-					
+
 
 			vkCmdPipelineBarrier(commandBuffer.getHandle(),
 					VK_PIPELINE_STAGE_TRANSFER_BIT,
 					VK_PIPELINE_STAGE_TRANSFER_BIT,
 					0, null, null, barrier);
-			
+
 			VkOffset3D src0_offset3D = VkOffset3D.calloc()
 					.x(0).y(0).z(0);
-			
+
 			VkOffset3D src1_offset3D = VkOffset3D.calloc()
 					.x(mipWidth).y(mipHeight).z(1);
-			
+
 			VkOffset3D dst0_offset3D = VkOffset3D.calloc()
 					.x(0).y(0).z(0);
-			
+
 			VkOffset3D dst1_offset3D = VkOffset3D.calloc()
 					.x(mipWidth/2).y(mipHeight/2).z(1);
-			
+
 			VkImageBlit.Buffer blit = VkImageBlit.calloc(1)
 					.srcOffsets(0, src0_offset3D)
 					.srcOffsets(1, src1_offset3D)
@@ -202,13 +203,13 @@ public class VkImageHelper {
 					.mipLevel(i)
 					.baseArrayLayer(0)
 					.layerCount(1);
-				
-			
+
+
 			vkCmdBlitImage(commandBuffer.getHandle(),
 					image, VK_IMAGE_LAYOUT_TRANSFER_SRC_OPTIMAL,
 					image, VK_IMAGE_LAYOUT_TRANSFER_DST_OPTIMAL,
 					blit, VK_FILTER_LINEAR);
-			
+
 			barrier.oldLayout(VK_IMAGE_LAYOUT_TRANSFER_DST_OPTIMAL)
 				.newLayout(VK_IMAGE_LAYOUT_TRANSFER_SRC_OPTIMAL)
 				.srcAccessMask(VK_ACCESS_TRANSFER_WRITE_BIT)
@@ -218,11 +219,11 @@ public class VkImageHelper {
 			    VK_PIPELINE_STAGE_TRANSFER_BIT,
 			    VK_PIPELINE_STAGE_TRANSFER_BIT,
 			    0, null, null, barrier);
-			
+
 			if (mipWidth > 1) mipWidth /= 2;
 			if (mipHeight > 1) mipHeight /= 2;
 		}
-		
+
 		barrier.subresourceRange()
 			.baseMipLevel(0)
 			.levelCount(mipLevels);
@@ -236,12 +237,12 @@ public class VkImageHelper {
 	    vkCmdPipelineBarrier(commandBuffer.getHandle(),
 	        VK_PIPELINE_STAGE_TRANSFER_BIT, finalDstStageMask,
 	        0, null, null, barrier);
-	    
+
 	    commandBuffer.finishRecord();
-	    
+
 	    SubmitInfo submitInfo = new SubmitInfo();
 	    submitInfo.setCommandBuffers(commandBuffer.getHandlePointer());
 	    submitInfo.submit(queue);
 	}
-	
+
 }
